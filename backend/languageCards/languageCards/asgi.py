b00c4b22c597dde1f2d8d9bb04f412def902b281
Michal <michal.plichta612@gmail.com>
2022-01-05 00:04:02 +0100

    add django backend

---
+++ backend/languageCards/languageCards/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for languageCards project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'languageCards.settings')
+
+application = get_asgi_application()
